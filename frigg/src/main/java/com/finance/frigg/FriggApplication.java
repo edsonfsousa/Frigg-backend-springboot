baa5458ac2683848ecfb041f5f54254aae1d7c05
ptf-dev02 <naua.holanda@pathfind.com.br>
2021-10-17 01:43:59 -0300

    Initial Commit

---
+++ frigg/src/main/java/com/finance/frigg/FriggApplication.java
@@ -0,0 +1,13 @@
+package com.finance.frigg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriggApplication.class, args);
+	}
+
+}
